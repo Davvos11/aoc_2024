d71cc64184cd2bd3822b3154be6e46d2e4670cd4
Davvos11 <vosdavid2@gmail.com>
2024-12-05 08:43:19 +0100

    Add day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,7 +1,9 @@
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::PathBuf;
+use itertools::Itertools;
 
 pub fn day05(input: &PathBuf) -> String {
     let file = File::open(input).expect(&format!("Can't open file {:?}", input));
@@ -13,18 +15,22 @@ pub fn day05(input: &PathBuf) -> String {
     for line in reader.lines() {
         let line = line.unwrap();
         if first {
-            if line.is_empty() { first = false; continue; }
-            let mut parsed = line.split("|").map(|s|s.parse().unwrap());
+            if line.is_empty() {
+                first = false;
+                continue;
+            }
+            let mut parsed = line.split("|").map(|s| s.parse().unwrap());
             if let (Some(a), Some(b)) = (parsed.next(), parsed.next()) {
                 rules.push((a, b));
             }
         } else {
-            lists.push(line.split(",").map(|s|s.parse().unwrap()).collect());
+            lists.push(line.split(",").map(|s| s.parse().unwrap()).collect());
         }
     }
-    
+
     let mut part1 = 0;
-    
+    let mut part2 = 0;
+
     for list in lists {
         let mut correct = true;
         for (rule_a, rule_b) in &rules {
@@ -37,12 +43,23 @@ pub fn day05(input: &PathBuf) -> String {
                 }
             }
         }
-        
+
         if correct {
             part1 += list[list.len() / 2]
+        } else {
+            let mut sorted = list.iter().sorted_by(|&&a, &&b| {
+                if rules.contains(&(a, b)) {
+                    Ordering::Less
+                } else if rules.contains(&(b, a)) {
+                    Ordering::Greater
+                } else {
+                    Ordering::Equal
+                }
+            });
+            part2 += sorted.nth(list.len() / 2).unwrap();
         }
     }
 
 
-    format!("Part one: {part1}\t Part two: ")
+    format!("Part one: {part1}\t Part two: {part2}")
 }
