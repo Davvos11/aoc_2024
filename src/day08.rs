a4bacbab63cb7aa3a5e31e569382e1a11ee06066
Davvos11 <vosdavid2@gmail.com>
2024-12-08 18:01:39 +0100

    8: cleanup

--- src/day08.rs
+++ src/day08.rs
@@ -3,7 +3,6 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::PathBuf;
-use itertools::Itertools;
 
 pub fn day08(input: &PathBuf) -> String {
     let file = File::open(input).expect(&format!("Can't open file {:?}", input));
@@ -29,20 +28,17 @@ fn solve(grid: &[Vec<char>]) -> (usize, usize) {
                     if c != '.' && c != '#' { Some((c, (y as isize, x as isize))) } else { None }
                 })
         });
+    
     let mut antennae = HashMap::new();
     for (c, coords) in antennae_iter {
         antennae.entry(c).or_insert(Vec::new()).push(coords);
     }
 
-    // println!("{antennae:?}");
-
     let mut antinodes1 = HashSet::new();
     let mut antinodes2 = HashSet::new();
 
-    for (c, coords) in antennae {
-        // Loop through all possible pairs
+    for  coords in antennae.values() {
         for (i, &(y1, x1)) in coords.iter().enumerate() {
-            // println!("{coords:?}");
             for &(y2, x2) in coords[i + 1..].iter() {
                 let distance = (y2 - y1, x2 - x1);
                 // Part1: add antinodes at equal distance 
@@ -57,8 +53,6 @@ fn solve(grid: &[Vec<char>]) -> (usize, usize) {
             }
         }
     }
-
-    // println!("{:?}", antinodes.iter().sorted().map(|(y,x)| (y, x)));
 
     let part1 = antinodes1.iter()
         .filter(|(y, x)| *y >= 0 && *y < rows && *x >= 0 && *x < cols)
