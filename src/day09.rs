fdb5a48d15e03e19773babd2078488e29d980504
Davvos11 <vosdavid2@gmail.com>
2024-12-09 21:10:59 +0100

    Add day 9 part 2

--- src/day09.rs
+++ src/day09.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::PathBuf;
+use itertools::Itertools;
 
 pub fn day09(input: &PathBuf) -> String {
     let file = File::open(input).expect(&format!("Can't open file {:?}", input));
@@ -12,34 +13,97 @@ pub fn day09(input: &PathBuf) -> String {
         .chars().map(|c| c.to_digit(10).unwrap());
 
     let mut empty_space = false;
-    let mut i = 0;
-    let mut disk: Vec<Option<usize>> = Vec::new();
-    for len in input {
-        if empty_space {
-            for _ in 0..len { disk.push(None) }
-        } else {
-            for _ in 0..len { disk.push(Some(i)) }
-            i += 1;
+    let mut disk1: Vec<Option<usize>> = Vec::new();
+    {
+        let mut i = 0;
+        for len in input {
+            if empty_space {
+                for _ in 0..len { disk1.push(None) }
+            } else {
+                for _ in 0..len { disk1.push(Some(i)) }
+                i += 1;
+            }
+            empty_space = !empty_space;
         }
-        empty_space = !empty_space;
     }
 
-    while let Some(item) = disk.pop() {
+    let mut disk2 = disk1.clone();
+
+    while let Some(item) = disk1.pop() {
         if item.is_some() {
-            if let Some(first_space) = disk.iter().position(|x| x.is_none()) {
-                disk[first_space] = item;
+            if let Some(first_space) = disk1.iter().position(|x| x.is_none()) {
+                disk1[first_space] = item;
             } else {
                 // No empty spaces found anymore, put the popped item back at the end
-                disk.push(item);
+                disk1.push(item);
                 break;
             }
         }
     }
 
-    let part1: usize = disk.iter().enumerate()
+    let part1: usize = disk1.iter().enumerate()
         .map(|(i, id)| i * id.unwrap()).sum();
 
-    format!("Part one: {part1}\t Part two: ")
+    let mut spaces = collect_empty_spaces(&disk2);
+    // Iterate over non-empty items, starting at the back
+    let mut current = 0;
+    let mut length = 0;
+    for item in disk2.clone().iter().rev()
+        .filter_map(|item| item.map(|v| v)) {
+        if item != current && length > 0 {
+            let item_idx = disk2.iter().position(|x| x == &Some(current)).unwrap();
+            // We found the end (or rather start) of the current item sequence
+            // Now check if it fits somewhere
+            if let Some(&empty_idx) = spaces.iter()
+                .find(|(_, size)| *size >= length)
+                .map(|(empty_idx, _)| empty_idx) {
+                if empty_idx < item_idx {
+                    // Move items there
+                    for j in 0..length {
+                        disk2[item_idx + j] = None;
+                        disk2[empty_idx + j] = Some(current);
+                    }
+                    // Update lookup
+                    spaces = collect_empty_spaces(&disk2);
+                }
+            }
+            // print_disk(&disk2);
+        }
+        if item != current {
+            current = item;
+            length = 0;
+        }
+        length += 1;
+    }
+
+    let part2: usize = disk2.iter()
+        .enumerate()
+        .filter_map(|(i, x)| x.map(|x| (i, x)))
+        .map(|(i, id)| i * id).sum();
+
+    format!("Part one: {part1}\t Part two: {part2}")
+}
+
+fn collect_empty_spaces(disk: &[Option<usize>]) -> Vec<(usize, usize)> {
+    let mut result = Vec::new();
+    let mut candidate = None;
+    let mut length = 0;
+    for (i, item) in disk.iter().enumerate() {
+        if item.is_some() {
+            if let Some(idx) = candidate {
+                result.push((idx, length));
+                candidate = None;
+            }
+        } else {
+            if candidate.is_none() {
+                candidate = Some(i);
+                length = 0;
+            }
+            length += 1;
+        }
+    }
+
+    result
 }
 
 #[allow(unused)]
