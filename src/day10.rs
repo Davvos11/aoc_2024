c833b34d7d946b3f91bbbc449868fdaaa252623b
Davvos11 <vosdavid2@gmail.com>
2024-12-10 09:21:28 +0100

    10.1: remove broken memoisation, it is correct now :)

--- src/day10.rs
+++ src/day10.rs
@@ -1,9 +1,9 @@
-use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::PathBuf;
-use itertools::Itertools;
 
 pub fn day10(input: &PathBuf) -> String {
     let file = File::open(input).expect(&format!("Can't open file {:?}", input));
@@ -35,21 +35,21 @@ pub fn day10(input: &PathBuf) -> String {
 }
 
 struct Map<'a> {
-    grid: &'a[Vec<u32>],
-    memo: HashMap<(usize, usize), HashSet<(usize, usize)>>,
+    grid: &'a [Vec<u32>],
     visited: HashSet<(usize, usize)>,
 }
 
 impl<'a> Map<'a> {
-    pub fn new(grid: &'a[Vec<u32>]) -> Self {
+    pub fn new(grid: &'a [Vec<u32>]) -> Self {
         Self {
-            grid, memo: HashMap::new(), visited: HashSet::new(),
+            grid,
+            visited: HashSet::new(),
         }
     }
     pub fn reset_visited(&mut self) {
         self.visited = HashSet::new();
     }
-    
+
     pub fn get_trail_ends(&mut self, y: usize, x: usize, current_h: u32) -> HashSet<(usize, usize)> {
         let mut result = HashSet::new();
         self.visited.insert((y, x));
@@ -63,11 +63,6 @@ impl<'a> Map<'a> {
                 }
                 if let Some(&h) = self.grid.get(y_new).and_then(|row| row.get(x_new)) {
                     if h == current_h + 1 {
-                        // First check cache
-                        if let Some(memo) = self.memo.get(&(y_new, x_new)) {
-                            result.extend(memo);
-                            continue;
-                        }
                         // Otherwise calculate and recurse
                         if h == 9 {
                             result.insert((y_new, x_new));
@@ -80,13 +75,8 @@ impl<'a> Map<'a> {
             }
         }
 
-        self.memo.insert((y, x), result.clone());
         result
     }
 }
 
 const DIRS: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
-
-fn rev_dir(y: isize, x: isize) -> (isize, isize) {
-    (-y, -x)
-}
